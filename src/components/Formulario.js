1019948b54b88e8a8ed4ab25a51290e356bf8b69
Ivan Urra <ivan.urra.wd@gmail.com>
2021-08-03 19:30:39 +0200

    formulario component

---
+++ src/components/Formulario.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const Formulario = () => {
+    return (
+        <form>
+            <h2>Agrega tus gastos aquí</h2>
+
+            <div className="campo">
+                <label>Nombre gasto:</label>
+                <input 
+                    type="text"
+                    className="u-full-width"
+                    placeholder="Ej. Transporte"
+                />
+            </div>
+            <div className="campo">
+                <label>Cantidad gasto:</label>
+                <input 
+                    type="number"
+                    className="u-full-width"
+                    placeholder="Ej. 300"
+                />
+            </div>
+            <input
+                    type="submit"
+                    className="u-full-width button-primary"
+                    value="AGREGAR"
+                />
+        </form>
+      );
+}
+ 
+export default Formulario;
