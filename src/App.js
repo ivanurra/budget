b949b7aaaa0231e2fd5542623ff9795c9d548256
Ivan Urra <ivan.urra.wd@gmail.com>
2021-08-03 18:59:51 +0200

    save budget

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import React, { useState } from 'react';
 import Pregunta from "./components/Pregunta";
 
 function App() {
+
+  // Definir el State
+  const [ presupuesto, guardarPresupuesto ] = useState(0);
+  const [ restante, guardarRestante ] = useState(0);
+
   return (
     <div className="container">
       <header>
         <h1>Presupuesto</h1>
         <div className="contenido-principal contenido">
-          <Pregunta />
+          <Pregunta
+            guardarPresupuesto={guardarPresupuesto}
+            guardarRestante={guardarRestante}
+          />
         </div>
       </header>
     </div>
